8c07c59c6f357eb71a4ab083c73b066d47fad1a3
wathsalaMadanasinghe <cwathsala123@gmail.com>
2024-06-11 15:41:34 +0530

    benefits_up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -9,6 +9,7 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
